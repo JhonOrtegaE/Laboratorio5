0dc96a6ae8e6a39b34ef2c1e69be9b81acff859f
JhonOrtegaE <142622512+JhonOrtegaE@users.noreply.github.com>
2023-09-19 21:59:43 -0500

    Add files via upload

---
+++ src/practicacampo5/PracticaCampo5.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicacampo5;
+
+/**
+ *
+ * @author USER
+ */
+public class PracticaCampo5 {
+
+    /**
+     * @param args the command line arguments
+     */
+  public static void main(String[] args) {
+         FrameEstudiantes estudiante = new FrameEstudiantes();
+         estudiante.setLocationRelativeTo(null);
+         estudiante.setVisible(true);
+    }
+    
+}
